c2ea0beed31e81dc959357e3c665129ff393142b
rocio <rg3113@yahoo.com>
2025-07-03 10:34:51 -0500

    add analytics

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import Provider from "./components/Provider";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -64,6 +65,7 @@ export default function RootLayout({ children }) {
           {/* <main className="pt-24 sm:pt-28">{children}</main> */}
         </Provider>
         <Footer />
+        <Analytics/>
       </body>
     </html>
   );
